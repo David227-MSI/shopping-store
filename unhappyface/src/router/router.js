5a3ac9f8cd9bc980e4a15f1d2e2599102d9a9a13
TonTonPaPacc <jfpoie13@gmail.com>
2025-04-27 15:32:16 +0800

    create basic project with home, 403, 404

---
+++ unhappyface/src/router/router.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+// 引用元件檔案
+// import 元件名 from 檔案位置
+import Home from '@/views/Home.vue'
+import NotFound from '@/views/errors/NotFound.vue'
+import Forbidden from '@/views/errors/Forbidden.vue'
+
+
+// 設定頁面參數 
+// path:網址, name:頁面名稱, component:元件名
+const routes = [
+  // 一般頁面
+  { path: '/', name: 'home', component: Home},
+  // { path: '/', name: '', component:  },
+
+  // 錯誤顯示頁面
+  { path: "/:pathMatch(.*)", component: NotFound, name: "notfound-link" },
+  { path: "/403", component: Forbidden, name: "forbidden-link" },
+]
+
+// 註冊頁面給Router管理
+const router = createRouter({
+  routes: routes,
+  history: createWebHistory(import.meta.env.BASE_URL)
+})
+
+export default router
---
+++ unhappyface/src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <main>
+    <h2>這是首頁</h2>
+  </main>
+</template>
+
+<script setup>
+</script>
