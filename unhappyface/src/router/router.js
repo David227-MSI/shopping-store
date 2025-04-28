8ebe95c5efff653510de5fe92139d213260bc1db
David227-MSI 😎 <a2272485@gmail.com>
2025-04-28 21:48:26 +0800

    feat: add order-related routes for cart, checkout, and order management

--- unhappyface/src/router/router.js
+++ unhappyface/src/router/router.js
@@ -6,19 +6,29 @@ import Home from '@/views/Home.vue'
 import NotFound from '@/views/errors/NotFound.vue'
 import Forbidden from '@/views/errors/Forbidden.vue'
 
+// 0428 order相關頁面
+import CartPage from '@/views/order/CartPage.vue'
+import CheckoutPage from '@/views/order/CheckoutPage.vue'
+import OrderCompletePage from '@/views/order/OrderCompletePage.vue'
+import OrdersPage from '@/views/order/OrdersPage.vue'
 
-// 設定頁面參數 
+// 設定頁面參數
 // path:網址, name:頁面名稱, component:元件名
 const routes = [
   // 一般頁面
   { path: '/', name: 'home', component: Home},
   // { path: '/', name: '', component:  },
-
   // 錯誤顯示頁面
   { path: "/:pathMatch(.*)", component: NotFound, name: "notfound-link" },
   { path: "/403", component: Forbidden, name: "forbidden-link" },
-]
 
+    // 訂單相關頁面
+  { path: '/cart', name: 'cart', component: CartPage },
+  { path: '/checkout', name: 'checkout', component: CheckoutPage },
+  { path: '/order/complete', name: 'order-complete', component: OrderCompletePage },
+  { path: '/member/orders', name: 'orders', component: OrdersPage }
+
+]
 // 註冊頁面給Router管理
 const router = createRouter({
   routes: routes,
