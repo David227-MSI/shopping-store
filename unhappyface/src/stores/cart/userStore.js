8fe43f47b207b3cb9da06bb7a443ab10db869b11
David227-MSI 😎 <a2272485@gmail.com>
2025-05-07 18:46:12 +0800

    feat(userStore): remove fetchUserInfo function and clean up user state management

--- unhappyface/src/stores/cart/userStore.js
+++ unhappyface/src/stores/cart/userStore.js
@@ -6,22 +6,6 @@ export const useUserStore = defineStore('user', () => {
     const user = ref({});        // 會員基本資料 (address, phone, name...)
     const isLoaded = ref(false); // 資料是否載入成功
 
-    async function fetchUserInfo() {
-        try {
-            const res = await axios.get('/ajax/secure/memberinfo');
-            if (res.success) {
-                user.value = res.data;
-                isLoaded.value = true;
-            } else {
-                user.value = {};
-                isLoaded.value = false;
-            }
-        } catch (error) {
-            user.value = {};
-            isLoaded.value = false;
-        }
-    }
-
     function setUser(data) {
         user.value = data;
         isLoaded.value = true;
@@ -31,13 +15,11 @@ export const useUserStore = defineStore('user', () => {
         user.value = {};
         isLoaded.value = false;
         localStorage.removeItem('token');
-        localStorage.removeItem('userId');
     }
 
     return {
         user,
         isLoaded,
-        fetchUserInfo,
         setUser,
         clearUser,
     };
